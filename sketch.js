d7d4b1ad2acc94ab6c15aa526d1a3ea661f817c9
S4ntiago22 <102492395+S4ntiago22@users.noreply.github.com>
2022-09-26 20:23:23 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var canvas;
+var backgroundImage;
+var eq1, eq2, eq3, eq4, eq5;
+var imgeq1, imgeq2, imgeq3, imgeq4,imgeq5;
+
+function preload(){
+  backgroundImage = loadImage("./image/imgfondo.jpg");
+  imgeq1 = loadImage("./image/imgeqbayerm.png")
+  imgeq2 = loadImage("./image/imgeqchelsea.png")
+  imgeq3 = loadImage("./image/imgeqmilan.png")
+  imgeq4 = loadImage("./image/imgeqpsg.png")
+  imgeq5 = loadImage("./image/imgeqrmadrid.png")
+  function setup(){
+   canvas = createCanvas(windowWidth, windowHeight); //cargar camara 22/09/22//
+  
+   //agregar equipos//
+   
+   eq1 = createSprite (skell -500,50,6,6);
+   eq1.addImage (imgeq1);
+   
+   eq2 = createSprite (skell -500,50,6,6);
+   eq2.addImage (imgeq2);
+  }
+}
+
+function draw(){
+ background(backgroundImage);
+  drawSprites();
+}
